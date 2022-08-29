ea1804d8640a16f71b6792063b2afd0d98c6bd6a
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2022-08-29 15:42:53 +0300

    use List and ListItem

--- tt-playbook-frontend/src/exercise/exercise-form/UnselectedExercises.tsx
+++ tt-playbook-frontend/src/exercise/exercise-form/UnselectedExercises.tsx
@@ -1,40 +1,44 @@
 import React, { ReactElement } from 'react'
-import styled from 'styled-components'
 import { Exercise } from '../exercise'
 import { useTranslation } from 'react-i18next'
 import IconButton from '@mui/material/IconButton'
 import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp'
-import { Typography } from '@mui/material'
+import { List, ListItem, ListItemText } from '@mui/material'
 
 interface Props {
   exercises: Exercise[]
   onSelectExercise: (exercise: Exercise) => void
 }
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
 export const UnselectedExercises = ({
   exercises,
   onSelectExercise,
 }: Props): ReactElement => {
   const { t, i18n } = useTranslation()
-
   return (
-    <ul>
+    <List>
       {exercises.map((exercise) => {
         return (
-          <Container key={exercise.id}>
-            <Typography>{exercise.title.getValue(i18n.language)}</Typography>
-            <IconButton onClick={() => onSelectExercise(exercise)}>
-              {t('unselectedExerciseList.select')}
-              <AddCircleOutlineSharpIcon />
-            </IconButton>
-          </Container>
+          <ListItem
+            key={exercise.id}
+            secondaryAction={
+              <IconButton
+                onClick={() => onSelectExercise(exercise)}
+                edge={'end'}
+              >
+                {t('unselectedExerciseList.select')}
+                <AddCircleOutlineSharpIcon />
+              </IconButton>
+            }
+            alignItems={'flex-start'}
+            sx={{ width: '20%' }}
+          >
+            <ListItemText
+              primary={exercise.title.getValue(i18n.language)}
+            ></ListItemText>
+          </ListItem>
         )
       })}
-    </ul>
+    </List>
   )
 }
